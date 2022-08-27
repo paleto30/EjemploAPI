29c949fcc0282e0893d407b4dca4a02d1fb6be27
Paleto <paleto0817@gmail.com>
2022-08-27 06:45:03 -0500

    Api desde la web

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    // aqui van todas las funciones
-    
+document.addEventListener('DOMContentLoaded', () => {
+    // aqui van todas las funciones    
     getPeliculas();
 });
 
 
 
-function getPeliculas(){
+function getPeliculas() {
     console.log("esta es la funcion peliculas");
-    const urlPeliculas = "data/movies.json";
+    const urlPeliculas = "https://www.omdbapi.com/?apikey=fcdb07e5&s=batman"
     fetch(urlPeliculas)
-    .then((response)=>{
-        //console.log("esto es una promesa");
-        //console.log(urlPeliculas);
-        return response.json(); 
-    })
-    .then((data)=>{
-        console.log(data);
-        renderMovies(data);
-    })
+        .then((response) => {
+            //console.log("esto es una promesa");
+            //console.log(urlPeliculas);
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            renderMovies(data);
+        })
     function renderMovies(movies) {
         const contMovies = document.querySelector("#contenedorPelis");
         let html = '';
-        movies.forEach(element => {
-            const {Title,Year,Poster} = element;
+
+        for (const x in movies.Search) {
+            console.log(x);
+            const { Title, Year, Poster } = movies.Search[x];
             html += `
                 <div class="card"  id="card">
                     <img src="${Poster}" alt="..." id="img">
@@ -33,14 +34,10 @@ function getPeliculas(){
 
                         <a href="#" class="btn btn-primary">Detalles</a>
                     </div>
-            </div>
+                </div>
             `;
-        contMovies.innerHTML = html;
-        });
-            
+            contMovies.innerHTML = html;      
+            }
     }
-
 }
-
-
 
